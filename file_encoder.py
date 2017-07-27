a0bedb994a6891f315b6b35a7386c0af7f4da5ff
jeffball <jeffball@dc949.org>
2017-07-27 19:10:44 -0400

    Update file encoder to read from bitstream as well

--- file_encoder.py
+++ file_encoder.py
@@ -1,5 +1,6 @@
 # This class will take bytes and then output a byte stream that can be written to a
 # clemency binary file.
+import binascii
 
 class ByteStream(object):
 
@@ -60,15 +61,71 @@ class ByteStream(object):
 
     return stream
 
+  def _parse_byte(self, stream, current_byte, current_num_bits):
+    num_new_bits = 9 - current_num_bits
+
+    need_extra_bit = 0
+    if num_new_bits > 8:
+      need_extra_bit = 1
+      num_new_bits = 8
+
+    value = 0
+    if current_num_bits != 0:
+      mask = (1 << current_num_bits) - 1
+      value = (current_byte & mask) << num_new_bits
+
+    offset = (8 - num_new_bits)
+    mask = ((1 << num_new_bits) - 1) << offset
+    new_byte = (ord(stream[0]) & mask) >> offset
+    value |= new_byte
+
+    current_byte = ord(stream[0])
+    current_num_bits = 8 - num_new_bits
+    stream = stream[1:]
+
+    if need_extra_bit:
+      current_byte = ord(stream[0])
+      current_num_bits = 7
+      value = (value << 1) | ((current_byte >> 7) & 1)
+      stream = stream[1:]
+
+    return stream, current_byte, current_num_bits, value
+
+
+  def add_from_byte_stream(self, stream, sizes):
+    current_byte = 0
+    current_num_bits = 0
+    for size in sizes:
+
+      if size == 3:
+        stream, current_byte, current_num_bits, middle = self._parse_byte(stream, current_byte, current_num_bits)
+        stream, current_byte, current_num_bits, most = self._parse_byte(stream, current_byte, current_num_bits)
+        stream, current_byte, current_num_bits, least = self._parse_byte(stream, current_byte, current_num_bits)
+        value = (most << 18) | (middle << 9) | least
+
+      if size == 2:
+        stream, current_byte, current_num_bits, least = self._parse_byte(stream, current_byte, current_num_bits)
+        stream, current_byte, current_num_bits, most = self._parse_byte(stream, current_byte, current_num_bits)
+        value = (most << 9) | least
+
+      elif size == 1:
+        stream, current_byte, current_num_bits, value = self._parse_byte(stream, current_byte, current_num_bits)
+
+      self.bytes.append((value, size))
+
+        
+
 
 if __name__ == "__main__":
   bs = ByteStream()
-  bs.add_bytes(0x5608040, 3) # The first 3 tri-words in the hello.bin
-  bs.add_bytes(0x00002b8, 3)
-  bs.add_bytes(0x5200780, 3)
+  bs.add_bytes(0x0000020, 3)
+  bs.add_bytes(0x0000020, 3)
+  bs.add_bytes(0x0000020, 3)
+  bs.add_bytes(0x0000020, 3)
+  bs.add_bytes(0x0000020, 3)
   out = bs.to_byte_stream()
-  print out.encode("hex")
-  
 
-
+  ibs = ByteStream()
+  byte_stream = "205608001002e00748c00000"
+  ibs.add_from_byte_stream(binascii.unhexlify(byte_stream), [3,3,3])
 
