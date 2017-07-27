f4520a2c0dad71a7ab25994c7b3e4161b7d6de7d
Grazfather <grazfather@gmail.com>
2017-07-27 10:22:42 -0700

    WeirdBytes: Store as list of ints

--- bits.py
+++ bits.py
@@ -11,28 +11,34 @@ class WeirdBytes(object):
         nbits = n*BITS_PER_BYTE
         nbyts = nbits / 8
 
-        self.buf = bitarray(endian='little')
-        self.buf.frombytes(buf[:nbyts])
+        ba = bitarray(endian='little')
+        ba.frombytes(buf[:nbyts])
+        self.buf = WeirdBytes.splitint(ba)
 
     def __len__(self):
         return len(self.buf)
 
     def __str__(self):
-        return str(self.split())
+        return str(self.buf)
 
-    def split(self):
-        """Split the bit array into proper 9 bit bytes."""
+    def __getitem__(self, key):
+        return self.buf[key]
+
+    @staticmethod
+    def split(ba):
+        """Split a bit array into proper 9 bit bytes."""
         l = []
-        for i in range(len(self)/9):
-            l.append(self.buf[i*9:(i+1)*9])
+        for i in range(len(ba)/9):
+            l.append(ba[i*9:(i+1)*9])
 
         return l
 
-    def toint(self):
-        """Split the bit array into ints."""
+    @staticmethod
+    def splitint(ba):
+        """Split a bit array into ints."""
         l = []
         # TODO: Endianness
-        for ba in self.split():
+        for ba in WeirdBytes.split(ba):
             l.append(ba[0] * 256 + ord(ba[1:].tobytes()))
 
         return l
@@ -47,16 +53,21 @@ class WeirdBytes(object):
         else:
             raise Exception("Can only swap 2 or 3 bytes")
 
+    def read(self, addr, size):
+        return self.buf[addr:addr+size]
+
+    def write(self, addr, buf):
+        self.buf = self.buf[:addr] + buf + self.buf[addr+len(buf):]
+
 
 if __name__ == "__main__":
     buf = b"123456789" # 9 bytes = 72 bits = 9 weird bytes
     b = WeirdBytes(buf)
 
     print b
-    print b.split()
-    print b.toint()
-    print WeirdBytes.swapbytes(b.split()[:2])
-    print WeirdBytes.swapbytes(b.toint()[:2])
-    print WeirdBytes.swapbytes(b.split()[:3])
-    print WeirdBytes.swapbytes(b.toint()[:3])
-    print WeirdBytes.swapbytes(WeirdBytes.swapbytes(b.toint()[:3]))
+    print b.read(3, 2)
+    b.write(3, [5, 6, 7])
+    print b
+    print WeirdBytes.swapbytes(b[:2])
+    print WeirdBytes.swapbytes(b[:3])
+    print WeirdBytes.swapbytes(WeirdBytes.swapbytes(b[:3]))
